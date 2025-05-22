e5250850e0a188ed52f97bc89d1c44209d382593
Leila <damovand@yahoo.com>
2025-05-22 14:00:59 -0400

    Capstone project Module 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
-    # path for get dealerships
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    # path for get dealer
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for dealer details
