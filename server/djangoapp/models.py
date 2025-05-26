7400fe1a6aa597a62a7db53dea7e2cb5399532ed
Leila <damovand@yahoo.com>
2025-05-26 14:21:14 -0400

    Lab5 Capstone Project

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -30,6 +30,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
@@ -49,4 +50,4 @@ class CarModel(models.Model):
     # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation 
+        return self.name  # Return the name as the string representation
